86168a8208ca164e543609a762b17a7ea62655bf
chenjiangsong <274815408@qq.com>
2017-07-31 01:42:49 +0800

    init 初始化webpack+babel工作环境

---
+++ build/dev.js
@@ -0,0 +1,39 @@
+var express = require('express')
+var webpack = require('webpack')
+var webpackConf = require('./webpack.config')
+
+var app = express()
+var compiler = webpack(webpackConf)
+
+var devMiddleware = require('webpack-dev-middleware')(compiler, {
+  quiet: true
+})
+
+var hotMiddleware = require('webpack-hot-middleware')(compiler, {
+  log: () => {}
+})
+
+compiler.plugin('compilation', function (compilation) {
+  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
+    hotMiddleware.publish({ action: 'reload' })
+    cb()
+  })
+})
+
+app.use(devMiddleware)
+
+app.use(hotMiddleware)
+
+app.listen(8080)
+
+// webpack(webpackConf, function (err, stats) {
+//   if (err) throw err
+  
+//   process.stdout.write(stats.toString({
+//     colors: true,
+//     modules: false,
+//     children: false,
+//     chunks: false,
+//     chunkModules: false
+//   }) + '\n\n')
+// })
